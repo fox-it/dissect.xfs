25b67819a0dc443ec4942a10ea2938c5a641f4c8
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2025-01-24 10:30:17 +0100

    Change linter to Ruff (#40)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,34 @@
+from __future__ import annotations
+
 import gzip
-import os
+from pathlib import Path
+from typing import TYPE_CHECKING, BinaryIO
 
 import pytest
 
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
-def absolute_path(filename):
-    return os.path.join(os.path.dirname(__file__), filename)
+
+def absolute_path(filename: str) -> Path:
+    return Path(__file__).parent / filename
 
 
-def gzip_file(filename):
+def gzip_file(filename: str) -> Iterator[BinaryIO]:
     with gzip.GzipFile(absolute_path(filename), "rb") as fh:
         yield fh
 
 
 @pytest.fixture
-def xfs_bin():
+def xfs_bin() -> Iterator[BinaryIO]:
     yield from gzip_file("data/xfs.bin.gz")
 
 
 @pytest.fixture
-def xfs_sparse_bin():
+def xfs_sparse_bin() -> Iterator[BinaryIO]:
     yield from gzip_file("data/xfs_sparse.bin.gz")
 
 
 @pytest.fixture
-def xfs_bigtime_bin():
+def xfs_bigtime_bin() -> Iterator[BinaryIO]:
     yield from gzip_file("data/xfs_bigtime.bin.gz")
